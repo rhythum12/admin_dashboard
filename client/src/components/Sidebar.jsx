4d852884b8255fed3b68e4b136830ca8e7044575
Ashim <ashim.adhikari86@gmail.com>
2023-01-07 09:41:50 +0545

    made SideBar and changes tp index at Layout

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
+import {
+    SettingsOutlined,
+    ChevronLeft,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutlined,
+    PublicOutlined,
+    PointOfSaleOutlined,
+    TodayOutlined,
+    CalendarMonthOutlined,
+    AdminPanelSettingsOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined
+} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import profileImage from "assets/profile.jpeg";
+
+const Sidebar = ({
+    drawerWidth,
+    isSidebarOpen,
+    setIsSidebarOpen,
+    isNonMobile,
+}) => {
+    const {pathname} =  useLocation();
+    const [active, setActive] = useState("");
+    const navigate = useNavigate();
+    const theme = useTheme();
+
+    useEffect(() => {
+        setActive(pathname.substring(1));
+      }, [pathname]);
+  return <Box component="nav">
+     {/* TODO: video 1 hour 10 minutes*/}
+  </Box>
+};
+
+export default Sidebar
