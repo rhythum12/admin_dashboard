4a1c6296bbf260d8ee644dc370fe848a525ccbe9
Ashim <ashim.adhikari86@gmail.com>
2023-01-14 09:35:45 +0545

    Data Injection and changes to nav-bar

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
-import globalReducer from "state"
-import { Provider } from 'react-redux';
-import { setupListeners } from '@reduxjs/toolkit/query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "state/api.js";
- 
 
 const store = configureStore({
   reducer: {
@@ -15,16 +14,14 @@ const store = configureStore({
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefault) => getDefault().concat(api.middleware),
-   
 });
 setupListeners(store.dispatch);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>
 );
