17be40c8dedff757f78666dc729ffa0cf00f9722
Ashim <ashim.adhikari86@gmail.com>
2023-01-19 20:44:23 +0545

    Server Changes and Maintenance

---
+++ server/controllers/data.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const data = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+data.propTypes = {
+
+}
+
+export default data
