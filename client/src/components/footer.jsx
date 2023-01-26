b544bcadd6546afc1d9216944d77df11fd67bea7
Ashim <ashim.adhikari86@gmail.com>
2023-01-26 22:13:45 +0545

    footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+const footer = () => {
+  return (
+    <Box component="Footer">
+        <Typography>
+            What is this 
+        </Typography>
+    </Box>
+  )
+}
+
+export default footer
