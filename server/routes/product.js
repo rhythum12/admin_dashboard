6bed70d793ac1e6ee8dd10c8106734e2b252eadf
Ashim <ashim.adhikari86@gmail.com>
2023-02-03 21:30:14 +0545

    server maintenance

--- server/routes/product.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getProducts } from "../controllers/client.js";
-
-const router = express.Router();
-
-router.get("/products", getProducts);
-
-export default router;
- 
